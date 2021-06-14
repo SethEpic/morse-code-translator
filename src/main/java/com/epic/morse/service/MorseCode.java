d750eeb9c3be2682ba0739ac19c8fd3038da4ac4
sarmbruster <Sarmbruster@clearent.com>
2021-06-14 14:37:52 -0500

    #128 - Added more code, fixed some issues with american morse code, added default separators for american morse code

--- src/main/java/com/epic/morse/service/MorseCode.java
+++ src/main/java/com/epic/morse/service/MorseCode.java
@@ -7,14 +7,16 @@ import static com.epic.morse.service.Utils.multiSpaceRegex;
 
 public final class MorseCode {
     private static final ValidationService validationService = new ValidationServiceImpl();
+    private static final String empty = "";
 
     private MorseCode() {
     }
 
     public static String convertToMorseCode(String message) {
         validationService.validateTextToMorseCode(message);
-        final String wordSeparator = MorseCodeConfig.getInstance().getWordSeparator();
-        final String letterSeparator = MorseCodeConfig.getInstance().getLetterSeparator();
+        MorseCodeConfig morseCodeConfig = MorseCodeConfig.getInstance();
+        final String wordSeparator = morseCodeConfig.getWordSeparator();
+        final String letterSeparator = morseCodeConfig.getLetterSeparator();
         return covertTextToMorseCode(message, wordSeparator, letterSeparator);
     }
 
@@ -43,12 +45,12 @@ public final class MorseCode {
         return morseCodeBuilder.toString().trim();
     }
 
-    private static String convertMorseCodeToText(String morseCode, String wordSeparatorRegex, String letterSeparatorRegex) {
+    private static String convertMorseCodeToText(String morseCode, String wordSepRegex, String letterSepRegex) {
         StringBuilder convertedToText = new StringBuilder();
-        String[] words = morseCode.trim().split(wordSeparatorRegex);
+        String[] words = morseCode.trim().split(wordSepRegex);
 
         for (String word : words) {
-            String[] letters = word.split(letterSeparatorRegex);
+            String[] letters = word.split(letterSepRegex);
             for (String letter : letters) {
                 convertedToText.append(MorseCodeSearcher.getTextCharacter(letter));
             }
@@ -58,20 +60,20 @@ public final class MorseCode {
         return convertedToText.toString().trim();
     }
 
-    private static void appendSpace(StringBuilder morseCodeBuilder, String wordSeparator, String letterSeparator) {
-        if (morseCodeBuilder.lastIndexOf(letterSeparator) == (morseCodeBuilder.length() - letterSeparator.length())) {
-            morseCodeBuilder.replace(morseCodeBuilder.length() - letterSeparator.length(), morseCodeBuilder.length(), "");
+    private static void appendSpace(StringBuilder morseCode, String wordSeparator, String letterSeparator) {
+        if (morseCode.lastIndexOf(letterSeparator) == (morseCode.length() - letterSeparator.length())) {
+            morseCode.replace(morseCode.length() - letterSeparator.length(), morseCode.length(), empty);
         }
-        morseCodeBuilder.append(wordSeparator);
+        morseCode.append(wordSeparator);
     }
 
-    private static void removeSeparatorsAtEnd(StringBuilder morseCodeBuilder, String wordSeparator, String letterSeparator) {
-        if (morseCodeBuilder.lastIndexOf(letterSeparator) == (morseCodeBuilder.length() - letterSeparator.length())) {
-            morseCodeBuilder.replace(morseCodeBuilder.length() - letterSeparator.length(), morseCodeBuilder.length(), "");
+    private static void removeSeparatorsAtEnd(StringBuilder morseCode, String wordSeparator, String letterSeparator) {
+        if (morseCode.lastIndexOf(letterSeparator) == (morseCode.length() - letterSeparator.length())) {
+            morseCode.replace(morseCode.length() - letterSeparator.length(), morseCode.length(), empty);
         }
 
-        if (morseCodeBuilder.lastIndexOf(wordSeparator) == (morseCodeBuilder.length() - wordSeparator.length())) {
-            morseCodeBuilder.replace(morseCodeBuilder.length() - wordSeparator.length(), morseCodeBuilder.length(), "");
+        if (morseCode.lastIndexOf(wordSeparator) == (morseCode.length() - wordSeparator.length())) {
+            morseCode.replace(morseCode.length() - wordSeparator.length(), morseCode.length(), empty);
         }
     }
 }
